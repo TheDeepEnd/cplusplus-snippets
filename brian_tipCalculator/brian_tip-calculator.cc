3021d4afddcb67ccd36b99affe475e2ddc2f6894
Brian Mayeshiro <brianmay@hawaii.edu>
2016-04-16 01:26:49 +0000

    add brian_tipCalculator

--- brian_tipCalculator/brian_tip-calculator.cc
+++ brian_tipCalculator/brian_tip-calculator.cc
@@ -16,13 +16,11 @@ void calculateTip(float billTotal, float tipPercent)
 int main()
 {
     float billTotal = 100;
-    float tipPercent = 0.15;
+    float tipPercent = 15;
     
     cout << "This program is meant to calculate your tip" << endl;
-    cout << "Your total bill is: " << billtotal << endl;
+    cout << "Your total bill is: " << billTotal << endl;
     cout << "Your tip percentage is: " << tipPercent << endl;
-    
-    // cout << "Please enter a value: $ (do not include currency)" << endl;
     
     calculateTip(billTotal, tipPercent);
     
